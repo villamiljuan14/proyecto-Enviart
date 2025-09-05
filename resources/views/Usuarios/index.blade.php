97d5f9651e1f7bfd4b08ee3856f584052bb52c7a
villamiljuan14 <villamiljuan14@gmail.com>
2025-09-04 21:12:41 -0500

    datatable

--- resources/views/Usuarios/index.blade.php
+++ resources/views/Usuarios/index.blade.php
@@ -9,44 +9,67 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
 
-                <table class="table-auto w-full border-collapse border border-blue-400 shadow-lg">
-                    <thead class="bg-blue-600 text-black">
-                        <tr>
-                            <th class="border border-blue-400 px-4 py-2">Email</th>
-                            <th class="border border-blue-400 px-4 py-2">Nombre</th>
-                            <th class="border border-blue-400 px-4 py-2">Apellido</th>
-                            <th class="border border-blue-400 px-4 py-2">Estado</th>
-                            <th class="border border-blue-400 px-4 py-2">Rol (ID)</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($usuarios as $usuario)
-                            <tr class="hover:bg-blue-50">
-                                <td class="border border-blue-400 px-4 py-2">{{ $usuario->EMAIL_USUARIO }}</td>
-                                <td class="border border-blue-400 px-4 py-2">
-                                    {{ $usuario->PRIMER_NOMBRE_USUARIO }}
-                                    {{ $usuario->SEGUNDO_NOMBRE_USUARIO }}
-                                </td>
-                                <td class="border border-blue-400 px-4 py-2">
-                                    {{ $usuario->PRIMER_APELLIDO_USUARIO }}
-                                    {{ $usuario->SEGUNDO_APELLIDO_USUARIO }}
-                                </td>
-                                <td class="border border-blue-400 px-4 py-2 text-center">
-                                    @if($usuario->ESTADO_USUARIO === 1)
-                                        <span class="px-2 py-1 bg-green-200 text-green-800 rounded">Activo</span>
-                                    @else
-                                        <span class="px-2 py-1 bg-red-200 text-red-800 rounded">Inactivo</span>
-                                    @endif
-                                </td>
-                                <td class="border border-blue-400 px-4 py-2 text-center">
-                                    {{ $usuario->ROL_ID_ROL }}
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                <table id="usuarios" class="table-auto w-full border-collapse border border-blue-400 shadow-lg">
+    <thead class="bg-blue-600 text-black">
+        <tr>
+            <th class="border border-blue-400 px-4 py-2">ID</th>
+            <th class="border border-blue-400 px-4 py-2">Email</th>
+            <th class="border border-blue-400 px-4 py-2">Nombre</th>
+            <th class="border border-blue-400 px-4 py-2">Apellido</th>
+            <th class="border border-blue-400 px-4 py-2">Estado</th>
+            <th class="border border-blue-400 px-4 py-2">Rol (ID)</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($usuarios as $usuario)
+            <tr class="hover:bg-blue-50">
+                <td class="border border-blue-400 px-4 py-2">{{ $usuario->ID_USUARIO }}</td>
+                <td class="border border-blue-400 px-4 py-2">{{ $usuario->EMAIL_USUARIO }}</td>
+                <td class="border border-blue-400 px-4 py-2">
+                    {{ $usuario->PRIMER_NOMBRE_USUARIO }}
+                    {{ $usuario->SEGUNDO_NOMBRE_USUARIO }}
+                </td>
+                <td class="border border-blue-400 px-4 py-2">
+                    {{ $usuario->PRIMER_APELLIDO_USUARIO }}
+                    {{ $usuario->SEGUNDO_APELLIDO_USUARIO }}
+                </td>
+                <td class="border border-blue-400 px-4 py-2 text-center">
+                    @if($usuario->ESTADO_USUARIO === 1)
+                        <span class="px-2 py-1 bg-green-200 text-green-800 rounded">Activo</span>
+                    @else
+                        <span class="px-2 py-1 bg-red-200 text-red-800 rounded">Inactivo</span>
+                    @endif
+                </td>
+                <td class="border border-blue-400 px-4 py-2 text-center">
+                    {{ $usuario->ROL_ID_ROL }}
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
 
-            </div>
-        </div>
-    </div>
+{{-- jQuery + DataTables (CDN) --}}
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.8/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.2/css/buttons.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.8/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.2/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+
+<script>
+    $(function () {
+        $('#usuarios').DataTable({
+            pageLength: 20,
+            dom: 'Bfrtip',
+            language: {
+                url: 'https://cdn.datatables.net/plug-ins/1.13.8/i18n/es-ES.json'
+            },
+            buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+        });
+    });
+</script>
 </x-app-layout>
